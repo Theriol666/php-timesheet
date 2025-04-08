aa01b6a4263da4b5ffcee1912b22a38010b04207
Raffaele Bortolotto <rbortolotto@alpenite.com>
2025-04-08 17:39:14 +0200

    init

---
+++ Readme.md
@@ -0,0 +1,66 @@
+# ::PHP TIMESHEET PARSER::
+
+## A cosa serve?
+Si tratta di uno script in PHP che interpreta il file timesheet.txt al fine di ricavare da esso la somma delle ore svolte nei vari progetti; e formattare le attività segnate in ciascuna riga al fine di eseguire un copia-incolla delle informazioni.
+
+## Cosa richiede?
+- PHP 7.4
+
+## Come funziona?
+Lo script analizza il file e da esso ricava alcune informazioni, quali: progetto, ore segnate per ciasuna attività, calcolo di queste ore, mese di riferimento. Al fine di una corretta lettura il file di testo deve seguire un certo formato.
+
+All'interno del file "timesheet.php", a riga 46, è possibile impostare il percorso del file da importare (al momento si aspetta che il file sia nella stessa cartella dello script PHP).
+
+Usando il file timesheet.txt di esempio si notano delle sezioni specifiche per ciascuna riga:
+- ################################################## > usato come stacco per dividere i vari mesi (ignorato in fase di elaborazione, è solo di abbellimento);
+- [04/29 09:15] > questa è la riga che usa lo script per capire che sta per elaborare una giornata e ne ricava la data (04/29) tramite regex;
+- Progetto 1: @smartworking: attività svolta: 3h | #secondaAttività: attivtà svolta: 1h > questa riga da indicazione di tutte le attività svolte, con relative tempistiche, di un progetto ("Progetto 1"). Lo script da questa riga capirà quale è il progetto a cui abbiamo assegnato le ore e le userà per fare un conteggio totale. Inserendo il simbolo del cancelletto (#) lo script riconoscerà che questo è l'identificativo di un task e alla fine dell'elaborazione terrà conto del conteggio di tutte le ore per questo specifico task.
+
+E' possibile aggiunge quanti progetti si vogliono, purché questi siano uno per riga, come riportato nel file timesheet.txt.
+
+In fase di conteggio ore lo script convertirà i minuti come segue:
+- 15m = 25
+- 30m = 50
+- 45m = 75
+- 60m = 0
+
+Nel caso il valore dei minuti non dovesse essere tondo come elencato, il valore mostrato sarà con virgola.
+
+Quando si esegue lo script senza parametri questo in automatico mosterà gli orari del mese corrente e userà come file di riferimento quello che si chiama "timesheet.txt".
+
+Da CLI è inoltre possibile passare i seguenti parametri:
+- period: filltro per trovare le attività segnate nel mese che ci interessa, definito con un valore a due cifre;
+- file: il nome del file da dover aprire in alternativa a timesheet.txt.
+
+### Da eseguire tramite comando
+`php timesheet.php (period="NN") (file="file.txt")`
+
+### Da eseguire tramite file CMD
+
+**Linux:**
+Si può creare un file .sh per eseguire in automatico il file php, come da esempio:
+`````
+#!bin/bash
+clear
+TIMESHEET_FILE_TEXT="/abs/path/to/timesheet/timesheet.txt"
+TIMESHEET_FILE_PHP="/abs/path/to/timesheet/timesheet.php"
+nano "${TIMESHEET_FILE_TEXT}"
+php7.4 "${TIMESHEET_FILE_PHP}" #period=09
+`````
+
+**Windows:**
+Da Windows è possibile fare un file bat che può sfruttare il file .sh tramite WSL2 oppure creare un bat per eseguire direttamente il codice.
+
+Nel primo caso lo script sarebbe fatto così:
+`````
+@echo off
+:timesheet
+cls
+bash "/abs/path/to/timesheet/timesheet.sh"
+set /P restart="Restart (y/*)? "
+if "%restart%" == "y" (
+  GOTO timesheet
+) else (
+  exit
+)
+`````
---
+++ timesheet.php
@@ -0,0 +1,342 @@
+<?php
+/** 
+Get CLI/WEB args
+@var array $args
+**/
+$args = getArgs();
+/**
+Get month to show (current as default)
+@var string $period
+**/
+$period = $args['period']??date('m', time());
+@define(PERIOD, $period);
+/**
+Set the regex rule to find start value for daily activities
+@const string PERIOD_REGEX
+**/
+const PERIOD_REGEX = "/\[%s\/(.\d) (.\d):(.\d)\]/";
+/**
+Map time values
+@const array TIME_VAR
+**/
+const TIME_VAR = [
+	"h" => "1",
+	"m" => "60",
+];
+
+/**
+Set thee regex rule to find all tasks
+@const string TASK_PATTERN
+**/
+CONST TASK_PATTERN = "/(#(?:[^\S]|\w)+)\:/";
+/**
+Set task separator simble
+@const string TASK_SEPARATOR
+**/
+CONST TASK_SEPARATOR = "|";
+
+$tasks = [];
+
+//CONST SORT = "desc";
+
+/**
+Get file rows for elaboration
+@var array $source
+**/
+$source = file($args['file']?? __DIR__ . '/timesheet.txt');
+
+if($source){
+	
+	// Parsing main variables
+	$projectTime = "";
+	$currentDay = 0;
+	$lastDay = 0;
+	$startProjectString = getPeriodRegex(PERIOD);
+	$projectsTotalTime = [];
+	$dayTotalTime = 0;
+	$today = date("m/d");
+	// Rows cycle
+	foreach($source as $lineIndex => $line){
+
+		// skip not usable rows
+		if(isToSkip($line)){ continue; }
+		// check if the current row contains a starting daily activities information
+		if(isStartTime($line, $lineIndex)){
+
+			// show current total day of today
+			if($dayTotalTime > 0 && ($lastDay === 0 || PERIOD."/".$lastDay == $today)){
+				showDailyTotal();
+			}
+		
+			writeString("------------------------------------------");
+			writeString(PERIOD."/".$currentDay.":\n");
+			writeProjectTime("");
+			continue;
+		
+		}else{
+			// break the script if the current row refers to a previous month thant current
+			if(breakByPeriod($line)){
+				// show current total day of today if it's the first report of the month
+				if($dayTotalTime > 0 && ($lastDay == 0 || PERIOD."/".$lastDay == $today)){
+					showDailyTotal();
+				}
+				
+				break;
+			}
+		}
+		
+		// check if the parser is elaborating a daily activities
+		if(isStartTraking()){
+			// get and write information for current project
+			writeProjectTime($line);
+			continue;
+		}
+	}
+	
+	if(!empty($projectsTotalTime)){
+		writeString("***************************");
+		writeString("Total time worked:");
+		foreach($projectsTotalTime as $project => $time){
+			writeString("- ".$project.": ". $time."h");
+			if(!isset($tasks[$project])){
+				continue;
+			}
+			
+			$totalTasks = 0;
+			foreach($tasks[$project] as $task => $taskTime){
+				writeString("  - ".$task.": ". $taskTime."h");
+				$totalTasks += $taskTime;
+			}
+			
+			$other = $time - $totalTasks;
+			if($other > 0){
+				writeString("  - Other: ". $other."h");
+			}
+		}
+	}
+}
+
+/**
+Skip the row if it's empty or equals to filtered values
+@param string $line
+@return bool
+**/
+function isToSkip($line){
+	
+	$toSkip = [
+		"##################################################",
+		""
+	];
+	$line = trim($line);
+	return empty($line) || in_array($line,$toSkip);
+}
+
+function isStartTime($line, $lineIndex){
+	global $currentDay, $lastDay, $startProjectString;
+
+	if(preg_match($startProjectString, $line)){
+		preg_match($startProjectString, $line, $match);
+		if($currentDay !== $match[1]){
+			$lastDay = $currentDay;
+		}
+		$currentDay = $match[1];
+		
+		return true;
+	}
+	
+	return false;
+}
+
+/**
+Verify if the script is elaborating the same day
+@global string $currentDay
+@global string $lastDay
+@return bool
+**/
+function isStartTraking(){
+	global $currentDay, $lastDay;
+	
+	if($currentDay !== $lastDay){ return true; }
+	else { return false; }
+}
+
+
+/**
+Saves the time values for each task to sum them, and clean the ouput string from time tracked
+@param string $line
+@return strng
+**/
+function writeProjectTime($line){
+	$totalTime = [];
+	
+	$lineExploded = explode(": ", $line);
+	$project = array_shift($lineExploded);
+	unset($lineExploded);
+	
+	$filteredLines = str_replace([$project, "@smartworking:", "@office:"],"",$line);
+	$tasks = array_filter(explode(TASK_SEPARATOR, $filteredLines));
+	
+	foreach($tasks as $taskLine){
+		
+		// find task
+		preg_match_all(TASK_PATTERN, $taskLine, $matchedTasks);
+		$hasTask = !empty($matchedTasks[1]);
+		
+		foreach(array_keys(TIME_VAR) as $time){
+			$regex = "/.[[:digit:]]".$time."/";
+			preg_match_all($regex, $taskLine, $match);
+			if(!empty(array_filter($match))){
+				
+				if($hasTask){
+					$match[0] = [ $matchedTasks[1][0]  => $match[0][0] ];
+				}
+				$totalTime[] = $match[0];
+			}
+			$line = trim($line);
+			$line = preg_replace($regex, "", $line);
+			$line = str_replace(":  |", " |", $line);
+			$line = str_replace(": |", " |", $line);
+			$line = str_replace(":|", " |", $line);
+			if (substr($line, -1, 1) === ":") {
+				$line = substr($line, 0, -1);
+			}
+		}
+	}
+	
+	$line .= "\n";
+
+	writeString($line, false);
+
+	calcTotalTime($totalTime,$line,$project);
+}
+
+/**
+Caulcate project's time traked and print it
+@param array $totalTime
+@param string $line
+@param string $project
+@return void
+**/
+function calcTotalTime($totalTime,$line,$project){
+	global $projectsTotalTime, $tasks, $dayTotalTime;
+	
+	if(empty($totalTime)){
+		return $line;
+	}
+
+	
+	$timeSpent = 0;
+	
+	foreach($totalTime as $times){
+
+		foreach($times as $index => $time){
+			
+			$taskTime = 0;
+			foreach(TIME_VAR as $timeUnit => $timeValue){
+				if(strpos($time, $timeUnit) !== false){
+					$time = (int) str_replace($timeUnit,"",$time);
+					if($timeUnit == "h"){
+						$taskTime += $time * $timeValue;
+					}elseif($timeUnit == "m"){
+						$taskTime += $time / $timeValue;
+					}
+				}
+			}
+			if(strpos($index,"#") !== false){
+				if(!isset($tasks[$project][$index])){
+					$tasks[$project][$index] = 0;
+				}
+				$tasks[$project][$index] += $taskTime;
+			}
+			$timeSpent += $taskTime;
+		}
+	}
+	
+	if(!isset($projectsTotalTime[$project])){
+		$projectsTotalTime[$project] = 0;
+	}
+	$projectsTotalTime[$project] += $timeSpent;
+	$dayTotalTime += $timeSpent;
+	writeString("> Total: " .$timeSpent. "h\n");
+}
+
+/**
+Verify if the script is parsing a new month lower than period selected
+@param string $line
+@return bool
+**/
+function breakByPeriod($line){
+	
+	$period = PERIOD;
+	
+	$prevPeriod = getPeriodRegex(str_pad($period - 1, 2, '0', STR_PAD_LEFT));
+	
+	if(preg_match($prevPeriod, $line)){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+/**
+Print in CLI the ouput of the script and check if use \n or not
+@param string $string
+@param bool $useN
+@return void
+**/
+function writeString($string, $useN = true){
+	echo $useN ? $string."\n" : $string;
+}
+
+/**
+Return a regex string used to check date information
+@param string $period
+@return string
+**/
+function getPeriodRegex($period){
+	return sprintf(PERIOD_REGEX, $period);
+}
+
+function showDailyTotal(){
+	global $dayTotalTime;
+	
+	writeString("");
+	writeString("Daily total: ". $dayTotalTime. "h");
+	writeString("");
+	$dayTotalTime = 0;
+}
+
+/**
+Verify if args are passed on command execution
+@return array
+**/
+function getArgs(){
+	$validArgs = [
+		'period',
+		'file'
+	];
+		
+	$runner = isset($_SERVER['argv']) && $_SERVER['argv'] > 1 ? $_SERVER['argv'] : $_GET;
+
+	$args = [];
+
+	foreach($runner as $argIndex => $arg){
+		
+		if($argIndex === 0){
+			continue;
+		}
+		
+		try{
+			list($argKey, $argValue) = explode("=" , $arg);
+			if(in_array($argKey, $validArgs)){
+				$args[$argKey] = $argValue;
+			}
+			
+		}catch(Exception $e){
+			unset($e);
+			writeString("Arg not valid: ".$arg);
+		}
+	}
+	
+	return $args;
+}
